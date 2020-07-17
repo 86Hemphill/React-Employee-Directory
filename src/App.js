1e9cd462595715450f67b4787c630243359b07a4
Logan Hemphill <logan.hemphill86@gmail.com>
2020-07-17 08:37:18 -0400

    added employees, fixed component bug

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
               salary={employee.salary}
               catchphrase={employee.catchphrase}
             />
-          ))};
+          ))}
       </Wrapper>
   )};
 }
