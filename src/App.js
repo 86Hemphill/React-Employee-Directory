34b2715fe0e55a6a7f3a4882b9023277fb625c23
Logan Hemphill <logan.hemphill86@gmail.com>
2020-07-23 18:52:25 -0400

    Changed card layout to a table

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
 import React, { Component } from "react";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import EmployeeCard from "./components/EmployeeCard";
 import employees from "./employees.json";
+import "./App.css";
 
 class App extends Component {
 
   state = {
-    employees
-  };
+        employees,
+        filteredEmployees: employees
+      };
+
+  sortByName() {
+    const sortedEmployees = this.state.employees.slice().sort((a, b) => {
+      if(a.name > b.name) {
+        return 1;
+      } else if(a.name < b.name) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    debugger;
+    this.setState({ filteredEmployees: sortedEmployees });
+  }
 
   render() {
     return (
-      <Wrapper>
-        <Title>Adventure Bay City Employee Directory</Title>
-        {this.state.employees.map(employee => (
-            <EmployeeCard
-              id={employee.id}
-              key={employee.id}
-              name={employee.name}
-              image={employee.image}
-              title={employee.title}
-              salary={employee.salary}
-              catchphrase={employee.catchphrase}
-            />
-          ))}
-      </Wrapper>
-  )};
+      <div className="container table-responsive">
+              <table className="table">
+                <thead>
+                  <tr className="table-primary">
+                    <th scope="col"></th>
+                    <th scope="col" onClick={this.sortByName.bind(this)}>Name</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Salary</th>
+                    <th scope="col">Catchphrase</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.filteredEmployees.map(employee => (
+                    <tr key={employee.id}>
+                      <td><img className="tableImg" src={employee.image}/></td>
+                      <td>{employee.name}</td>
+                      <td>{employee.title}</td>
+                      <td>{employee.salary}</td>
+                      <td>{employee.catchphrase}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              </div>
+    );
+  }
 }
 
 export default App;
